f88293e0c12f9d4646961bdf91e112cf447fc26b
Admin <Admin@Runner-PC>
2017-07-20 13:50:30 +0300

    human logic

--- test5/src/test5/Human.java
+++ test5/src/test5/Human.java
@@ -3,4 +3,27 @@ package test5;
 public class Human {
 	private double a;
 	private int age;
+	
+	private String name;
+	
+	
+	
+
+	public Human(double a, int age, String name) {
+		super();
+		this.a = a;
+		this.age = age;
+		this.name = name;
+	}
+
+
+
+
+	@Override
+	public String toString() {
+		return "Human [a=" + a + ", age=" + age + ", name=" + name + "]";
+	}
+
+
+	
 }
